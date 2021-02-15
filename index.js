0f902fa8d9113248b81d56f816553eb960c845c2
Ajay <ajay@Ajays-Air.lan>
2021-02-15 18:10:40 +0800

    Changes for building index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 try {
-  const root = path.join(process.env.GITHUB_WORKSPACE, './')
+  const root = path.join(process.env.GITHUB_WORKSPACE, './').join('package.json')
   const data = JSON.parse(fs.readFileSync(root))
   core.setOutput("product", data.name);
 } catch (error) {
