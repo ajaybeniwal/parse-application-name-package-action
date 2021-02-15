d22b66e88c29b7a92ecfc89c01904bb9c4fdbeb2
Ajay <ajay@Ajays-Air.lan>
2021-02-15 18:30:01 +0800

    Changes for adding index file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   const root = path.join(process.env.GITHUB_WORKSPACE, './');
   const package = path.join(root,'package.json');
   const data = JSON.parse(fs.readFileSync(package))
-  const[product, mfename] = data.split("/");
+  const[product, mfename] = data.name.split("/");
   core.setOutput("product", product);
   core.setOutput("mfename", mfename);
 } catch (error) {
