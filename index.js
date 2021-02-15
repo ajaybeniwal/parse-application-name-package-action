48e34dede3183049a6fc41a8b532c29f2c73ad5c
Ajay <ajay@Ajays-Air.lan>
2021-02-15 17:31:15 +0800

    Changes for parsing the json file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 try {
   // `who-to-greet` input defined in action metadata file
   const time = (new Date()).toTimeString();
+  core.info(process.env.GITHUB_WORKSPACE);
   core.setOutput("product", time);
 } catch (error) {
   core.setFailed(error.message);
