b7b4930fd5394057137b77c4305d18e692f08ecd
Ajay <ajay@Ajays-Air.lan>
2021-02-15 18:16:59 +0800

    Changes for index file to add new action

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 try {
-  const root = path.join(process.env.GITHUB_WORKSPACE, './').join('package.json')
-  const data = JSON.parse(fs.readFileSync(root))
+  const root = path.join(process.env.GITHUB_WORKSPACE, './');
+  const package = path.join(root,'package.json');
+  const data = JSON.parse(fs.readFileSync(package))
   core.setOutput("product", data.name);
 } catch (error) {
   core.setFailed(error.message);
