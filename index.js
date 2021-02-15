ec8b9424a7ee9ac608d0fecd32daf261f76b7ef4
Ajay <ajay@Ajays-Air.lan>
2021-02-15 17:47:08 +0800

    Changes for adding debug information

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
 try {
+  const root = path.join(process.env.GITHUB_WORKSPACE, './')
   // `who-to-greet` input defined in action metadata file
   const time = (new Date()).toTimeString();
-  core.info(process.env.GITHUB_WORKSPACE);
+  core.info(root);
+  console.debug(root);
   core.setOutput("product", time);
 } catch (error) {
   core.setFailed(error.message);
